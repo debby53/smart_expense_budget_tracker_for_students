ef5b8d971b7d981d5f7d6b4a952e2627bc144bb6
ISHIMWE Deborah <ishimwedebby53@gmail.com>
2025-06-30 16:43:05 +0200

    m

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,20 @@ plugins {
 android {
     namespace = "com.example.smart_expense_budget_tracker_for_students"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    dependencies {
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+        // ... other dependencies like implementation(project(":..."))
     }
 
     defaultConfig {
--- android/app/src/main/kotlin/com/example/smart_expense_budget_tracker_for_students/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/smart_expense_budget_tracker_for_students/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.smart_expense_budget_tracker_for_students
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // Empty class body is fine
+}
